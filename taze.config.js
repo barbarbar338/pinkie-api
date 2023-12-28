c5cf4851ac353e7099b62bf7cea745d0c7dc42c5
Barış DEMİRCİ <hi@338.rocks>
2023-12-28 12:07:22 +0300

    👷 fly.io workflow

--- taze.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "taze";
-
-export default defineConfig({
-	// we don't want to use beta version of these packages
-	exclude: ["nestjs-tsx-views", "express-tsx-views"],
-});
